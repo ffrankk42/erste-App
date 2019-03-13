a0045b05304d5cf17c2b36b7135181f271bd8483
ffrankk42 <mathe.internet@icloud.com>
2019-03-13 19:54:55 +0100

    sdjljjj

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-import { YachtclubPage } from '../pages/yachtclub/yachtclub.module'
+import { Yachtclub } from '../pages/yachtclub/yachtclub';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
